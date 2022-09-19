b37cdcf7f7b4635d04b95e0c6df53d84814780b9
Sunny <trajputtrana@gmail.com>
2022-09-19 12:39:04 +0500

    4th test

--- app.js
+++ app.js
@@ -13,3 +13,16 @@
 // test3;
 // const numrev = (num) => (num = Number(String(num).split("").reverse().join("")));
 // console.log(numrev(654321));
+
+
+
+// test4
+// const firstupper = (str) => {
+//    const arr = str.toLowerCase().split(" ")
+//     for(var i = 0;i < arr.length; i++){
+//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//     }
+//     const arr2 = arr.join(" ")
+//     return arr2
+// }
+// console.log(firstupper( 'i have now lOndon'))
